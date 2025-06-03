e9ae53b6e0efbe00877b79987db835dbfbaa5692
emchem1 <207917399+emchem1@users.noreply.github.com>
2025-06-03 20:45:42 +0100

    Update scripts-rsvp.js

--- scripts-rsvp.js
+++ scripts-rsvp.js
@@ -1,7 +1,3 @@
-form.websiteentry("sumbit", function (f) {
-  if passwordInput = "eucalyptus";
-  return href home}
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("rsvpform");
